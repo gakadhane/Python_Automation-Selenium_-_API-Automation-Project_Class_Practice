c4c642dc3767ef6a5fcbb8a3a4e8d5bec201c53f
Gaurav Kadhane <gakadhane@gmail.com>
2024-11-19 11:18:40 +0530

    Practical program work

---
+++ november/19112024_Python_Basic/Practice055_Functions.py
@@ -0,0 +1,17 @@
+def first_part_last_name():
+    pass  # in future i will complete this functions
+
+
+# Part 1
+def greet_to_all_of_you():
+    print("Hello, Everyone")
+
+
+# Part 2
+def greet():
+    print("Yes")
+    greet_to_all_of_you()
+
+
+# Part 3
+greet()
