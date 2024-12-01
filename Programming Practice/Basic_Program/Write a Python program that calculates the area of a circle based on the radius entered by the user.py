78d1476bcc19245d5d304867dbc27a256510c843
Gaurav Kadhane <gakadhane@gmail.com>
2024-12-01 12:18:38 +0530

    Practical program work

---
+++ Programming Practice/Basic_Program/Write a Python program that calculates the area of a circle based on the radius entered by the user.py
@@ -0,0 +1,6 @@
+radius = float(input("Enter the radius:\n"))
+pi = 3.14
+Area = pi * radius * 2
+
+print("Area of circle with\t" +str(radius)+ "\tis:\t" +str(Area))
+    
---
+++ Programming Practice/Basic_Program/Write a Python program to display the current date and time.py
@@ -0,0 +1,7 @@
+import datetime
+
+now = datetime.datetime.now()
+
+print("Current date and time:")
+
+print(now.strftime("%Y-%M-%D %H:%M:%S"))
