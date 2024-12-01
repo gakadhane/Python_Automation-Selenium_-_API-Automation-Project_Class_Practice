cb7c17e98701915d065c04f7dd1de40330a65afa
Gaurav Kadhane <gakadhane@gmail.com>
2024-12-01 12:22:20 +0530

    Practical program work

--- Programming Practice/Basic_Program1/Write a Python program that calculates the area of a circle based on the radius entered by the user.py
+++ Programming Practice/Basic_Program1/Write a Python program that calculates the area of a circle based on the radius entered by the user.py
@@ -2,5 +2,4 @@ radius = float(input("Enter the radius:\n"))
 pi = 3.14
 Area = pi * radius * 2
 
-print("Area of circle with\t" +str(radius)+ "\tis:\t" +str(Area))
-    
+print("Area of circle with\t" + str(radius) + "\tis:\t" + str(Area))
